33970469f00e81de0e4d6b6a0395892e16561077
“JefferyyYong” <jefferyyth-wm17@student.tarc.edu.my>
2021-04-14 19:28:29 +0800

    update action bar

--- app/src/main/java/com/example/mysweethome/signUp.kt
+++ app/src/main/java/com/example/mysweethome/signUp.kt
@@ -3,7 +3,10 @@ package com.example.mysweethome
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
+import androidx.appcompat.app.ActionBar
+
 import kotlin.math.sign
 
 class signUp : AppCompatActivity() {
@@ -11,11 +14,28 @@ class signUp : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sign_up)
 
+        setTitle("Sign Up")
+
         val signUpForm = findViewById<Button>(R.id.signUpConBtn)
+        var actionBar = getSupportActionBar()
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+        }
 
         signUpForm.setOnClickListener {
             val intent = Intent(this, login::class.java)
             startActivity(intent)
         }
+    } override fun onContextItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
+
+
 }
